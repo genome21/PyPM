40e651acc5a143d8cec0e215555360cfe71b2f6b
Robert Wilkins III <thecainvestors@gmail.com>
2016-02-04 05:14:23 -0600

    Update start.py to fix issue in the WITH statements

--- start.py
+++ start.py
@@ -4,21 +4,20 @@ import datetime
 with open('updates.txt', 'r') as morningstart:
      morningstart.read()
      linelist = morningstart.readline()
-
-operstamp = getpass.getuser()
-timestamp = datetime.datetime.now().strftime('%Y%m%d-%H:%M:%S')
+     operstamp = getpass.getuser()
+     timestamp = datetime.datetime.now().strftime('%Y%m%d-%H:%M:%S')
 
 with open('updates.txt', 'a') as dailyupdates:
-print('This is your last update: ')
-print (linelist)
-print('What are your updates?')
-typedupdates = input()
-nline = "\n"
-iline = " -- "
-dailyupdates.write(operstamp)
-dailyupdates.write(iline)
-dailyupdates.write(timestamp)
-dailyupdates.write(nline)
-dailyupdates.write(typedupdates)
-ender = "\nEOF-------------------------------------------EOF\n\n"
-dailyupdates.write(ender)
+     print('This is your last update: ')
+     print (linelist)
+     print('What are your updates?')
+     typedupdates = input()
+     nline = "\n"
+     iline = " -- "
+     dailyupdates.write(operstamp)
+     dailyupdates.write(iline)
+     dailyupdates.write(timestamp)
+     dailyupdates.write(nline)
+     dailyupdates.write(typedupdates)
+     ender = "\nEOF-------------------------------------------EOF\n\n"
+     dailyupdates.write(ender)
